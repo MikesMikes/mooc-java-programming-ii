7be1b38759214c8ecbed992e0a100034f41daf7f
madnessguy67 <michaelkaliber15@gmail.com>
2021-09-03 22:54:50 +0100

    Button and TextField task

---
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -0,0 +1,30 @@
+package borderpane;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class BorderPaneApplication extends Application{
+
+
+    public static void main(String[] args) {
+        launch(BorderPaneApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane pane = new BorderPane();
+        pane.setTop(new Label("NORTH"));
+        pane.setRight(new Label("EAST"));
+        pane.setBottom(new Label("SOUTH"));
+        
+        Scene scene = new Scene(pane);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
