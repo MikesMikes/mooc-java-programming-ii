7d098af13a620b7341cffed523c54e9b653acd4a
madnessguy67 <michaelkaliber15@gmail.com>
2021-09-09 15:24:13 +0100

    cli ask user for title

--- part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,14 +1,18 @@
 package title;
 
+import java.util.Scanner;
 import javafx.application.Application;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Title?");
+        String title = "--title=" + scanner.nextLine();
+//        System.out.println(title);
         Application.launch(UserTitle.class,
-            "--something=Once upon a time",
-            "--course=Title");
+            title);
     }
 
 }
