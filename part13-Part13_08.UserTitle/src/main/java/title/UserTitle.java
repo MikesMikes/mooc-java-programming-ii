f725d9a74e79943c5ab09aef47ca86a180c1987f
madnessguy67 <michaelkaliber15@gmail.com>
2021-09-09 15:45:51 +0100

    com

--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,19 @@
 package title;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
+
+public class UserTitle extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parameters params = getParameters();
+        String something = params.getNamed().get("title");
+        
+        stage.setTitle(something);
+        stage.show();
+    }
 
 
 }
